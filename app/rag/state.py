85bf088989ff380f7a4b45717747351098ed30e7
RudySeo <gun100491@gmail.com>
2025-08-23 20:48:09 +0900

    feat(QueryState): QueryState 추가

--- app/rag/state.py
+++ app/rag/state.py
@@ -1,8 +1,17 @@
-from typing import TypedDict
-from langchain.docstore.document import Document
+from typing import List
+from typing_extensions import NotRequired, TypedDict
+from langchain_community.vectorstores import FAISS
 
 
 class GraphState(TypedDict):
+    file_path: str
+    documents: NotRequired[List]
+    chunks: NotRequired[List]
+    vectorstore: NotRequired[FAISS]
+
+
+class QueryState(TypedDict):
     question: str
-    documents: list[Document]
-    answer: str
+    vectorstore: FAISS
+    context: NotRequired[str]
+    answer: NotRequired[str]
