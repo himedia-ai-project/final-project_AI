c6090cc2f599cc7814613be281694b74753fb6d8
RudySeo <gun100491@gmail.com>
2025-08-27 16:56:24 +0900

    feat(State) :  pdf_id, store_path 추가

--- app/rag/state.py
+++ app/rag/state.py
@@ -1,13 +1,18 @@
-from typing import List
+from typing import Annotated, List
+from numpy import long, number
 from typing_extensions import NotRequired, TypedDict
 from langchain_community.vectorstores import FAISS
+from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
+from langgraph.graph.message import add_messages
 
 
 class GraphState(TypedDict):
     file_path: str
+    pdf_id: NotRequired[str]
     documents: NotRequired[List]
     chunks: NotRequired[List]
     vectorstore: NotRequired[FAISS]
+    store_path: NotRequired[str]
 
 
 class QueryState(TypedDict):
