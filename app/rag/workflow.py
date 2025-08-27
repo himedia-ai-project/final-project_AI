459e15aaf9a84c1e8efefbcc54e36662eaaca055
RudySeo <gun100491@gmail.com>
2025-08-27 16:57:31 +0900

    feat(WorkFlow) : save_vectorstore 추가

--- app/rag/workflow.py
+++ app/rag/workflow.py
@@ -4,26 +4,29 @@ from app.rag.nodes import (
     generate_answer,
     load_pdf,
     retrieve_context,
+    save_vectorstore,
     split_chunks,
 )
 from app.rag.state import GraphState, QueryState
 
 
-# PDF Workflow 
+# PDF Workflow
 pdf_workflow = StateGraph(GraphState)
 pdf_workflow.add_node("load_pdf", load_pdf)
 pdf_workflow.add_node("split_chunks", split_chunks)
 pdf_workflow.add_node("create_vectorstore", create_vectorstore)
+pdf_workflow.add_node("save_vectorstore", save_vectorstore)
 
 pdf_workflow.set_entry_point("load_pdf")
 pdf_workflow.add_edge("load_pdf", "split_chunks")
 pdf_workflow.add_edge("split_chunks", "create_vectorstore")
-pdf_workflow.set_finish_point("create_vectorstore")
+pdf_workflow.add_edge("create_vectorstore", "save_vectorstore")
+pdf_workflow.set_finish_point("save_vectorstore")
 
 pdf_graph = pdf_workflow.compile()
 
 
-# Query Workflow 
+# Query Workflow
 query_workflow = StateGraph(QueryState)
 query_workflow.add_node("retrieve_context", retrieve_context)
 query_workflow.add_node("generate_answer", generate_answer)
