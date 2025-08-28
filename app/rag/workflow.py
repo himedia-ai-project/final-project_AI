ca00ca52ba951ffff9183ba36f77d6c765e047b5
RudySeo <gun100491@gmail.com>
2025-08-28 22:10:42 +0900

    feat(Workflow): summarize_history,onditional edge  추가

--- app/rag/workflow.py
+++ app/rag/workflow.py
@@ -1,11 +1,14 @@
 from langgraph.graph import StateGraph
 from app.rag.nodes import (
     create_vectorstore,
+    decide_next,
     generate_answer,
     load_pdf,
+    load_vectorstore,
     retrieve_context,
     save_vectorstore,
     split_chunks,
+    summarize_history,
 )
 from app.rag.state import GraphState, QueryState
 
@@ -25,14 +28,21 @@ pdf_workflow.set_finish_point("save_vectorstore")
 
 pdf_graph = pdf_workflow.compile()
 
-
 # Query Workflow
 query_workflow = StateGraph(QueryState)
+query_workflow.add_node("load_vectorstore", load_vectorstore)
 query_workflow.add_node("retrieve_context", retrieve_context)
+query_workflow.add_node("summarize_history", summarize_history)
 query_workflow.add_node("generate_answer", generate_answer)
+query_workflow.add_node("decide_next", decide_next)
 
-query_workflow.set_entry_point("retrieve_context")
-query_workflow.add_edge("retrieve_context", "generate_answer")
+query_workflow.set_entry_point("load_vectorstore")
+query_workflow.add_edge("load_vectorstore", "retrieve_context")
+
+# conditional edge 추가
+query_workflow.add_conditional_edges("retrieve_context", decide_next)
+
+query_workflow.add_edge("summarize_history", "generate_answer")
 query_workflow.set_finish_point("generate_answer")
 
 query_graph = query_workflow.compile()
